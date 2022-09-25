e6172e26c405e9079092d82a02dd90a7ec082eab
Evelyn Koo <r_evelynkoo@berkeley.edu>
2022-09-25 14:11:50 -0700

    updates

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
-chosen=$(printf "  Power Off\n  Restart\n  Lock" | rofi -dmenu -i -theme-str '@import "power.rasi"')
+chosen=$(printf "  Power Off\n  Restart\n  Logout \n  Lock" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
 	"  Power Off") poweroff ;;
 	"  Restart") reboot ;;
-	"  Lock") slock ;;
+	"  Logout") swaymsg exit ;;
+	"  Lock") swaylock --font monospace --effect-blur 7x5 --effect-vignette -0.5:0.5 --grace 2 --fade-in 0.2 ;;
 	*) exit 1 ;;
 esac
