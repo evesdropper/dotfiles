f2a5c9294f16799a219cf6acd58103d495ee5a39
Evelyn Koo <r_evelynkoo@berkeley.edu>
2022-12-29 19:55:40 -0800

    update generating snippets, clean up lua.lua

--- nvim/luasnip/lua.lua
+++ nvim/luasnip/lua.lua
@@ -1,24 +1,33 @@
+-- Lua Snippets, 90% of which are for neovim/luasnip config.
+
+--[
+-- Imports
+--]
+local autosnippet = ls.extend_decorator.apply(s, {snippetType = "autosnippet"})
+
 return {
+    --[
+    -- LuaSnip Snippets
+    --]
     -- format snippet 
     s("snipf", fmt([[ 
-    s({ trig='<>', name='<>', dscr='<>'},
+    <>({ trig='<>', name='<>', dscr='<>'},
     fmt(<>,
     { <> },
     { delimiters='<>' }
     )<>)<>,]],
-    { i(1, "trig"), i(2, "trig"), i(3, "dscr"), i(4, "fmt"), i(5, "inputs"), i(6, "<>"), i(7, "opts"), i(0)},
+    { c(1, {t("s"), t("autosnippet")}), i(2, "trig"), i(3, "trig"), i(4, "dscr"), i(5, "fmt"), i(6, "inputs"), i(7, "<>"), i(8, "opts"), i(0)},
     { delimiters='<>' }
     )),
     -- simple text snippet 
     s("snipt", fmt([[ 
-    s('<>', {t('<>')}<>
+    <>(<>, {t('<>')}<>
     <>)<>,]],
-    { i(1, "trig"), i(2, "text"), i(3, "opts"), i(4), i(0)},
+        { c(1, {t("s"), t("autosnippet")}), c(2, {t("trig"), sn(nil, {t("{trig='"), i(1), t("'}")})}), i(3, "text"), i(4, "opts"), i(5), i(0)},
     { delimiters='<>' }
     )),
-}, {
     -- add snippet conditions
-    s({ trig='sacd', name='snippet conditions for autosnippets', dscr='snippet condition for autosnippets'},
+    autosnippet({ trig='sacd', name='snippet conditions for autosnippets', dscr='snippet condition for autosnippets'},
     fmt([[{ condition=<> }]],
     { i(1, math) },
     { delimiters='<>' }
@@ -27,18 +36,31 @@ return {
     { i(1, "math"), rep(1) },
     { delimiters='<>' }
     )),
+    -- special stuff - snippet regex, hide, switch priority
+    autosnippet('sreg', {t('regTrig=true, hidden=true')}
+    ),
     s({ trig='sprio', name='snip priority', dscr='Autosnippet to set snippet priority' },
     fmt([[ 
     priority=<>]],
     { i(1, '1000') },
     { delimiters='<>' }
     )),
-    s('shide', {t('hidden=true')}
+    autosnippet('shide', {t('hidden=true')}
     ),
     -- add nvim keybinds
     s({ trig='nkey', name='nvim keybinds', dscr='add a nvim keybinds'},
     fmt([[keymap("<>", "<>", "<>", "<>")]],
     { i(1, "n"), i(2, "keybind"), i(3, "command"), i(4, "options") },
+    { delimiters='<>' }
+    )),
+    -- create lua function
+    s({ trig='funcl', name='lua function', dscr='creates a function in lua'},
+    fmt([[
+    function(<>) 
+    <>
+    end
+    ]],
+    { i(1, "args"), i(0) },
     { delimiters='<>' }
     ))
 }
