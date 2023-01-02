e7d1b6d570ae1dcb668372926a2eed11b77f9d7b
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-01-01 17:26:50 -0800

    add thing for md tex

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,35 +1,49 @@
 -- load maplocalleaders first
-require "random.vimtex"
-require "random.telescope"
-require "random.dashboard"
+require("random.vimtex")
+require("random.telescope")
+require("random.dashboard")
 
 -- core
-require "random.options"
-require "random.keymaps"
-require "random.plugins"
+require("random.options")
+require("random.keymaps")
+require("random.plugins")
 
--- appearance 
-require "random.colorscheme"
-require "random.icons"
-require "random.scrollbar"
-require "random.lualine"
-require "random.bufferline"
-require "random.presence"
+-- appearance
+require("random.colorscheme")
+require("random.icons")
+require("random.scrollbar")
+require("random.lualine")
+require("random.bufferline")
+require "random.winbar"
+require("random.navic")
+require("random.presence")
 
 -- code editing
-require "random.treesitter"
-require "random.autopairs"
-require "random.cmp"
-require "random.gitsigns"
+require("random.comments")
+require("random.treesitter")
+require("random.autopairs")
+require("random.lsp")
+require("random.illuminate")
+require("random.cmp")
+require("random.gitsigns")
+require("random.toggleterm")
+
+-- markdown moment 
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = {'*.md', "*.tex", "*.sty"},
+  group = group,
+  command = 'setlocal wrap'
+})
 
 -- luasnip
-ls = require "luasnip"
+ls = require("luasnip")
 ls.config.set_config({
-	history = true, --keep around last snippet local to jump back
-    enable_autosnippets = true,
-    store_selection_keys="<Tab>"})
-ls.filetype_extend("tex", { "lua", "python" })
-require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/luasnip/"})
+	history = true, -- keep around last snippet local to jump back
+	enable_autosnippets = true,
+	store_selection_keys = "<Tab>",
+})
+ls.filetype_extend("tex", { "cpp", "python" })
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/luasnip/" })
 --require("luasnip.loaders").edit_snippet_files {
 --  extend = function(ft, paths)
 --    if #paths == 0 then
@@ -42,4 +56,4 @@ require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/luasnip/"})
 --    return {}
 --  end
 --}
-vim.cmd[[silent command! LuaSnipEdit :lua require("luasnip.loaders").edit_snippet_files()]]
+vim.cmd([[silent command! LuaSnipEdit :lua require("luasnip.loaders").edit_snippet_files()]])
