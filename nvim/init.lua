b2d17d308e920f1ebad6534211c55e3f78edcfd5
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-03-03 01:58:38 -0800

    clean up init + use lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,62 +1,30 @@
--- load maplocalleaders first
-require("random.vimtex")
-require("random.telescope")
-require("random.dashboard")
+-- -- load maplocalleaders first
 
--- core
-require("random.options")
-require("random.keymaps")
-require("random.plugins")
+-- core 
+require("core.options")
+require("core.keymaps")
+require("core.lazy") -- plugin manager
+require("core.autocommands")
+
+-- core plugins 
 require("random.lsp")
-
--- appearance
-require("random.colorscheme")
-require("random.icons")
-require("random.scrollbar")
-require("random.lualine")
-require("random.bufferline")
-require "random.winbar"
-require("random.navic")
-require("random.presence")
-
--- code editing
-require("random.comments")
+require("random.cmp")
+require("random.luasnip")
+require("random.telescope")
 require("random.treesitter")
 require("random.autopairs")
-require("random.illuminate")
-require("random.cmp")
-require("random.gitsigns")
+require("random.surround")
+
+-- code editing 
+require("random.nvim-tree")
 require("random.toggleterm")
 
--- markdown moment 
-vim.api.nvim_create_autocmd('BufEnter', {
-  pattern = {'*.md', "*.tex", "*.sty"},
-  group = group,
-  command = 'setlocal wrap linebreak'
-})
+-- appearance 
+require("random.colorscheme")
+require("random.lualine")
+require("random.bufferline")
+require("random.winbar")
+require("random.navic")
 
--- luasnip
-ls = require("luasnip")
-ls.config.set_config({
-	history = true, -- keep around last snippet local to jump back
-	enable_autosnippets = true,
-	store_selection_keys = "<Tab>",
-})
-ls.filetype_extend("tex", { "cpp", "python" })
-require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
-local types = require("luasnip.util.types")
--- require'luasnip'.config.setup({
--- 	ext_opts = {
--- 		[types.choiceNode] = {
--- 			active = {
--- 				virt_text = {{"⊙", "GruvboxOrange"}}
--- 			}
--- 		},
--- 		[types.insertNode] = {
--- 			active = {
--- 				virt_text = {{"●", "GruvboxBlue"}}
--- 			}
--- 		}
--- 	},
--- })
-vim.cmd([[silent command! LuaSnipEdit :lua require("luasnip.loaders").edit_snippet_files()]])
+-- ft specific 
+require("random.vimtex") -- latex
