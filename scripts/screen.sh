86dc5ac86721f8faa2daca9ab717efe0929bf633
Evelyn Koo <r_evelynkoo@berkeley.edu>
2024-04-29 14:48:01 -0700

    update screenshot script to resize satty based on image size

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -26,5 +26,12 @@ echo $fileout
 # make screenshot directory
 [ -d "$screenshot_dir" ] || mkdir $screenshot_dir
 
+# get coordinates, which will be used to resize satty window
+coords=$(slurp) || exit
+mapfile -t coords_array < $(echo $coords | grep -Po "[x\s]\K([0-9]+)")
+xsize=$([[ ${coords_array[0]} -ge 1600 ]] && ${coords_array[0]} || 1600)
+ysize=$([[ ${coords_array[1]} -ge 900 ]] && ${coords_array[1]} || 900)
+
 # take screenshot and copy to clipboard
-grim -g "$(slurp)" - | satty --filename - --output-filename $fileout
+grim -g "$coords" - | satty --filename - --output-filename $fileout
+swaymsg for_window [app_id="satty"] resize set $xsize $ysize
