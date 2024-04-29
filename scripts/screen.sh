b5fdce97f741b43508a958b331e8be2cb98ed73e
Evelyn Koo <r_evelynkoo@berkeley.edu>
2024-04-29 02:06:13 -0700

    update screenshot utility - use satty for a better experience and have screenshots go to specific dirs

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,9 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
-# couple lines of code to take screenshots and save with a file name.
-screenshot_dir="$HOME/Pictures/Screenshots/"
-# fname= $(zenity --entry --title="Take a Screenshot" --text="Image Name:") || $(date +%Y_%m%d_%H%M%S)
-fileout="$screenshot_dir/$(zenity --entry --title="Take a Screenshot" --text="Image Name:").png"
-[ -d "$screenshot_dir" ]; grim -g "$(slurp)" $fileout
-swappy -f $fileout
-wl-copy < $fileout
+# new screenshot script
+# TODO: get script to work with course-specific directories
+DEFAULT_SCREENSHOT_DIR="$HOME/Pictures/Screenshots/"
+
+# get filename; filename conventions defined below
+# blank: temp/deleted screenshot 
+# [class]-[(n|h)] [name]: figure in class notes/hw 
+# [folder] [name]: save in [folder]
+# [name] must be one word, mostly for sanity reasons
+filename="$(zenity --entry --title="Take a Screenshot" --text="Image Name:")" || exit
+
+IFS=' '
+read -ra filenamearr <<< "$filename"
+screenshot_dir=$DEFAULT_SCREENSHOT_DIR
+if [[ ${#filenamearr[@]} -gt 1 ]]; then
+    screenshot_dir="$DEFAULT_SCREENSHOT_DIR${filenamearr[0]}/"
+    filename=${filenamearr[1]}
+    echo $screenshot_dir $filename
+fi
+
+fileout="$screenshot_dir$filename.png"
+echo $fileout
+
+# make screenshot directory
+[ -d "$screenshot_dir" ] || mkdir $screenshot_dir
+
+# take screenshot and copy to clipboard
+grim -g "$(slurp)" -
+wl-copy < -
+
+# edit screenshot 
+satty --filename - --output-filename "$screenshot_dir$filename.png"
