abe1d74ba2b32ecd0623ccc9cd24fd9f2a700d5e
Evelyn Koo <r_evelynkoo@berkeley.edu>
2024-05-01 05:57:26 -0700

    i have an obsession with screenshots

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -11,6 +11,10 @@ while [[ $# -gt 0 ]]; do
             FULL_SCREEN=YES
             shift 
             ;;
+        "-w"|--window) 
+            WINDOW=YES
+            shift
+            ;;
         "-d"|--delay)
             DELAY="$2"
             shift
@@ -25,21 +29,36 @@ done
 
 # get filename; filename conventions defined below
 # blank: temp/deleted screenshot 
-# [class]-[(n|h)] [name]: figure in class notes/hw 
 # [folder] [name]: save in [folder]
-# [name] must be one word, mostly for sanity reasons
+# @c (h)? [name]: save in current-course/(notes|hw)/figures/
+# @z [query] [name]: zoxide finds the top match, then places in that folder
+# [name] must be one word, mostly for sanity reasons - I dislike files with names like "some dumb file name.png"
 filename="$(zenity --entry --title="Take a Screenshot" --text="Image Name:")" || exit
 
 IFS=' '
 read -ra filenamearr <<< "$filename"
 screenshot_dir=$DEFAULT_SCREENSHOT_DIR
 if [[ ${#filenamearr[@]} -gt 1 ]]; then
-    screenshot_dir="$DEFAULT_SCREENSHOT_DIR${filenamearr[0]}/"
-    filename=${filenamearr[1]}
-    echo $screenshot_dir $filename
+    case ${filenamearr[0]} in
+        "@z")
+            screenshot_dir="$(zoxide query "${filenamearr[1]}" | sed 's/\\/\\\\/g;s/"/\\"/g')/"
+            ;;
+        "@c")
+            if [[ ${#filenamearr[@]} -eq 2 ]] then
+                screenshot_dir="$HOME/Documents/university/bachelor-3/current-course/notes/figures/"
+            else
+                screenshot_dir="$HOME/Documents/university/bachelor-3/current-course/hw/figures/"
+            fi
+            ;;
+        *)
+            screenshot_dir="$DEFAULT_SCREENSHOT_DIR${filenamearr[0]}/"
+            ;;
+    esac
+    fileout="$screenshot_dir${filenamearr[-1]}.png"
+else
+    fileout="$DEFAULT_SCREENSHOT_DIR${filenamearr[0]}.png"
 fi
 
-fileout="$screenshot_dir$filename.png"
 echo $fileout
 
 # make screenshot directory
@@ -47,17 +66,26 @@ echo $fileout
 
 # get coordinates, which will be used to resize satty window
 if [[ $FULL_SCREEN == "YES" ]]; then
+    if [[ -n "$DELAY" ]]; then 
+        sleep $DELAY
+    fi
     grim - | satty --filename - --output-filename $fileout
+    exit 0
+elif [[ $WINDOW == "YES" ]]; then 
+    coords=$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp)
 else
     coords=$(slurp) || exit
-    mapfile -t coords_array < $(echo $coords | grep -Po "[x\s]\K([0-9]+)")
-    xsize=$([[ ${coords_array[0]} -ge 1600 ]] && ${coords_array[0]} || 1600)
-    ysize=$([[ ${coords_array[1]} -ge 900 ]] && ${coords_array[1]} || 900)
 fi
 
+# delay handling
 if [[ -n "$DELAY" ]]; then 
     sleep $DELAY
 fi
+
+mapfile -t coords_array < $(echo $coords | grep -Po "[x\s]\K([0-9]+)")
+xsize=$([[ ${coords_array[0]} -ge 1600 ]] && ${coords_array[0]} || 1600)
+ysize=$([[ ${coords_array[1]} -ge 900 ]] && ${coords_array[1]} || 900)
+
 
 # # take screenshot and copy to clipboard
 grim -g "$coords" - | satty --filename - --output-filename $fileout
